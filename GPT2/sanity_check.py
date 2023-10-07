7cd8da46910e2f46992b23f1ba6f97365bf18bf2
RyanK0902 <txryank@stanford.edu>
2023-10-07 12:23:33 -0700

    correct framework

--- GPT2/sanity_check.py
+++ GPT2/sanity_check.py
@@ -3,52 +3,56 @@ from datasets import load_dataset
 from tqdm import tqdm
 import torch
 
-import ipdb
-
 # Load pre-trained model and tokenizer
-MASK_TOKEN = "<MASK>"
 model_name = "gpt2"  # options: "gpt2", "gpt2-medium", "gpt2-large", or "gpt2-xl"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-if tokenizer.add_special_tokens({"mask_token": MASK_TOKEN}) > 0:
-    print("=> <MASK> token added...")      # making sure GPT2 recognizes the <MASK> token
 
-model.resize_token_embeddings(len(tokenizer))
 model.eval()  
 model.to('cpu')
 dataset = load_dataset("cbt", "CN")
 
-def predict_missing_word(context, question, options):
-    #ipdb.set_trace()
-    mask_id = tokenizer.encode(MASK_TOKEN, add_special_tokens=False)[0]  # Get the token id for MASK_TOKEN
-    context_ids = tokenizer.encode(" ".join(context), return_tensors='pt')
-    question_ids = tokenizer.encode(question.replace("XXXXX", MASK_TOKEN), return_tensors='pt')
-    input_ids = torch.cat([context_ids, question_ids], dim=1)
-    mask_index = torch.where(input_ids == mask_id)[1].item()  
+def predict_missing_word(context, question, options, answer):
+    """
+    Task description from original paper:
+        Predict which of 10 possible choices for an omitted word is correct. 
+        Do this by computing the probability of each choice and the rest of the sentence conditioned on this choice according to the LM, 
+        and predict the one with the highest probability.
+    """
+    # prefix
+    pre_mask_text, post_mask_text = question.split("XXXXX")
+    context_text = " ".join([sent.strip() for sent in context])
+    input_prefix = context_text + " " + pre_mask_text.strip()
 
-    with torch.no_grad():
-        logits = model(input_ids, attention_mask=torch.ones_like(input_ids)).logits
-        logits_at_mask = logits[0, mask_index]
-        logits_at_mask[mask_id] = float('-inf')    # mask the MASK_TOKEN when predicting
-        predicted_token = torch.argmax(logits_at_mask).item()
+    # Calculate likelihood of the rest of the sentence for each option
+    best_option = None
+    best_score = float('-inf')
 
-    predicted_word = tokenizer.decode([predicted_token], skip_special_tokens=True)
-    print("question: ", question)
-    print("predicted word: ", predicted_word)
-    return predicted_word in options
+    for option in options:
+        candidate_text = input_prefix + option + post_mask_text
+        input_ids = tokenizer.encode(candidate_text, return_tensors='pt')
 
+        with torch.no_grad():
+            logits = model(input_ids).logits
+
+        score = logits[0].sum().item()
+        if score > best_score:
+            best_score = score
+            best_option = option
+
+    return best_option == answer  # Assuming the answer is a single word immediately after "XXXXX"
 
 def main():
     sentences_list = dataset['test']['sentences']
     question_list = dataset['test']['question']
     options_list = dataset['test']['options']
+    answer_list = dataset['test']['answer']
 
     correct_predictions = 0
-    total_samples = 10
-    #total_samples = len(dataset['test'])
+    total_samples = 50  # Change this to len(dataset['test']) for the full dataset
 
     for i in tqdm(range(total_samples), desc="Predicting", ncols=100):     
-        if predict_missing_word(sentences_list[i], question_list[i], options_list[i]):
+        if predict_missing_word(sentences_list[i], question_list[i], options_list[i], answer_list[i]):
             correct_predictions += 1
 
     accuracy = correct_predictions / total_samples
