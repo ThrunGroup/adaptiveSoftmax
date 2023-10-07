d160abb8040e7ae414addc674a048e4cbe19a798
RyanK0902 <txryank@stanford.edu>
2023-10-07 11:54:58 -0700

    predicting cbt set

--- GPT2/sanity_check.py
+++ GPT2/sanity_check.py
@@ -10,32 +10,31 @@ MASK_TOKEN = "<MASK>"
 model_name = "gpt2"  # options: "gpt2", "gpt2-medium", "gpt2-large", or "gpt2-xl"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-num_added_tokens = tokenizer.add_special_tokens({"additional_special_tokens": [MASK_TOKEN]})
-model.resize_token_embeddings(len(tokenizer))
+if tokenizer.add_special_tokens({"mask_token": MASK_TOKEN}) > 0:
+    print("=> <MASK> token added...")      # making sure GPT2 recognizes the <MASK> token
 
+model.resize_token_embeddings(len(tokenizer))
 model.eval()  
 model.to('cpu')
 dataset = load_dataset("cbt", "CN")
 
 def predict_missing_word(context, question, options):
-    mask_position = tokenizer.encode(MASK_TOKEN, add_special_tokens=False)[0]  # Get the token id for MASK_TOKEN
+    #ipdb.set_trace()
+    mask_id = tokenizer.encode(MASK_TOKEN, add_special_tokens=False)[0]  # Get the token id for MASK_TOKEN
     context_ids = tokenizer.encode(" ".join(context), return_tensors='pt')
     question_ids = tokenizer.encode(question.replace("XXXXX", MASK_TOKEN), return_tensors='pt')
     input_ids = torch.cat([context_ids, question_ids], dim=1)
+    mask_index = torch.where(input_ids == mask_id)[1].item()  
 
-    if input_ids.shape[1] > model.config.n_positions:
-        print("Input is too long for model")
-        return False
-
-    mask_index = torch.where(input_ids == mask_position)[1].item()  # Find the position of the MASK token
     with torch.no_grad():
-        output = model.generate(input_ids, 1)  # Give some space for generation but you can adjust
+        logits = model(input_ids, attention_mask=torch.ones_like(input_ids)).logits
+        logits_at_mask = logits[0, mask_index]
+        logits_at_mask[mask_id] = float('-inf')    # mask the MASK_TOKEN when predicting
+        predicted_token = torch.argmax(logits_at_mask).item()
 
-    ipdb.set_trace()
-    predicted_token = output[0][mask_index].item()
     predicted_word = tokenizer.decode([predicted_token], skip_special_tokens=True)
-    print("this is prediction: ", predicted_word)
-    print("these are options: ", options)
+    print("question: ", question)
+    print("predicted word: ", predicted_word)
     return predicted_word in options
 
 
