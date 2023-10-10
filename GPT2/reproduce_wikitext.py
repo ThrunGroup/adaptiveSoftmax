3d1f8f9efa643062af2b7951f78362362dd538ab
RyanK0902 <txryank@stanford.edu>
2023-10-10 11:07:09 -0700

    reproducing wikitext

--- GPT2/reproduce_wikitext.py
+++ GPT2/reproduce_wikitext.py
@@ -1,38 +1,41 @@
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from datasets import load_dataset
 import torch
 from tqdm import tqdm
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from datasets import load_dataset
 
-# Load dataset and model from huggingface
-dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
-model_name = "gpt2"
-model = GPT2LMHeadModel.from_pretrained(model_name).eval()
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+device = "cpu"
+model_id = "gpt2"
+model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
 
-# Prepare input tokens
-stride = 32
-max_length = 1024
-text = dataset["test"]["text"]
-inputs = tokenizer(text, return_tensors="pt", max_length=max_length, stride=stride, truncation=True)
+test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt")
+max_length = model.config.n_positions
+stride = 512
+seq_len = encodings.input_ids.size(1)
 
-# Remove potential empty sequences
-import ipdb; ipdb.set_trace()
-non_empty_indices = [i for i, input_ids in enumerate(inputs["input_ids"]) if len(input_ids) > 0]
-inputs["input_ids"] = inputs["input_ids"][non_empty_indices]
-inputs["attention_mask"] = inputs["attention_mask"][non_empty_indices]
+nlls = []
+prev_end_loc = 0
+for begin_loc in tqdm(range(0, seq_len, stride)):
+    end_loc = min(begin_loc + max_length, seq_len)
+    trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+    input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+    target_ids = input_ids.clone()
+    target_ids[:, :-trg_len] = -100
 
-# Compute the perplexity over the tokenized test set
-total_loss = 0.0
-num_chunks = len(inputs["input_ids"])
+    with torch.no_grad():
+        outputs = model(input_ids, labels=target_ids)
 
-with torch.no_grad():
-    for i in tqdm(range(num_chunks)):
-        input_ids = inputs["input_ids"][i].unsqueeze(0)
-        attention_mask = inputs["attention_mask"][i].unsqueeze(0)
-        outputs = model(input_ids, attention_mask=attention_mask, labels=input_ids)
-        total_loss += outputs.loss.item()
+        # loss is calculated using CrossEntropyLoss which averages over valid labels
+        # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+        # to the left by 1.
+        neg_log_likelihood = outputs.loss
 
-avg_loss = total_loss / num_chunks
-perplexity = torch.exp(torch.tensor(avg_loss)).item()
+    nlls.append(neg_log_likelihood)
 
-print(f"Perplexity: {perplexity}")
+    prev_end_loc = end_loc
+    if end_loc == seq_len:
+        break
+
+ppl = torch.exp(torch.stack(nlls).mean())
+print("perplexity is ", ppl)
