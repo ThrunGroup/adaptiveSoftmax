6e06b6d09214ee5c70dae81750ac0825c08bba5e
lukehan <ailikehuman@gmail.com>
2023-10-11 08:22:16 +0900

    Readability changes

--- adaptive_softmax/adasoftmax.py
+++ adaptive_softmax/adasoftmax.py
@@ -17,8 +17,6 @@ def approx_sigma_bound_nb(A, x, n_sigma_sample):
     for i in range(n_arms):
         sigma[i] = np.std(elmul[i])
 
-    #print(sigma)
-
     return x.shape[0] * np.median(sigma)
 
 @njit
@@ -31,7 +29,6 @@ def compute_mip_batch_topk_ver2_warm_nb(atoms, query, sigma, delta, batch_size=1
 
     dim = len(query)
     n_atoms = len(atoms)
-
     best_ind = np.empty(n_atoms, dtype=np.int64)
     found_indices_num = 0
 
@@ -46,17 +43,23 @@ def compute_mip_batch_topk_ver2_warm_nb(atoms, query, sigma, delta, batch_size=1
     max_index = np.argmax(mu)
     max_mu = mu[max_index]
     #d_used + 1(denominator) should be in the square root
-    C = np.divide(sigma * np.sqrt(2 * np.log(4 * n_atoms * d_used ** 2 / delta)),
-                  d_used + 1) if d_used is not None else np.zeros(n_atoms)
 
-    solution_mask = np.ones(n_atoms, dtype="bool") & (
-                mu + C >= max_mu - C[max_index]) if mu is not None else np.ones(n_atoms, dtype="bool")
+    C_numerator = sigma * 2 * np.log(4 * n_atoms * d_used**2 / delta)
+    C_denominator = d_used + 1
+
+    if d_used is not None:
+        C = np.sqrt(C_numerator / C_denominator)
+    else:
+        C = np.zeros(n_atoms)
+
+    if mu is not None:
+        solution_mask = (mu + C) >= (max_mu - C[max_index])
+    else:
+        solution_mask = np.ones(n_atoms, dtype="bool")
     solutions = np.nonzero(solution_mask)[0]
-    # topk_indices = np.array([], dtype=np.int64)
 
     if len(solutions) <= k:
-        # topk_indices = np.append(topk_indices, solutions)
-        best_ind[found_indices_num: found_indices_num + len(solutions)] = solutions
+        best_ind[: len(solutions)] = solutions
         found_indices_num += len(solutions)
         solution_mask = np.logical_not(solution_mask)
         solutions = np.nonzero(solution_mask)[0]
@@ -67,16 +70,15 @@ def compute_mip_batch_topk_ver2_warm_nb(atoms, query, sigma, delta, batch_size=1
 
     C = np.divide(sigma * np.sqrt(2 * np.log(4 * n_atoms * d_used ** 2 / delta)), d_used + 1)
 
-    print(d_used)
-
     solution_mask_before = solution_mask
 
     brute_force_threshold = np.ceil(atoms.shape[0] * 0.05)
 
-    while (len(solutions) > brute_force_threshold and found_indices_num < k and np.max(
-            d_used) < dim - batch_size):  # TODO: computing max everytime may degrade performance
+    is_not_over_bruteforce_threshold = len(solutions) > brute_force_threshold
+    have_not_found_topk = found_indices_num < k
+    have_not_sampled_arm_fully = np.max(d_used) < dim - batch_size
 
-        #print("flag")
+    while is_not_over_bruteforce_threshold and have_not_found_topk and have_not_sampled_arm_fully:
 
         tmp = np.empty(np.sum(solution_mask))
 
@@ -90,8 +92,11 @@ def compute_mip_batch_topk_ver2_warm_nb(atoms, query, sigma, delta, batch_size=1
                                   d_used[solution_mask] + batch_size)
         n_samples += len(solutions) * batch_size
 
-        C = np.divide(sigma * np.sqrt(2 * np.log(4 * n_atoms * d_used ** 2 / delta)),
-                      d_used + 1)  # TODO: update confidence bound. This is when we're sampling one A_iJ * x_J at each round. Can and should be tighter than this -> divide with + batch size somehow?
+        C_numerator = sigma * 2 * np.log(4 * n_atoms * d_used ** 2 / delta)
+        C_denominator = d_used + 1
+
+
+        C = np.sqrt(np.divide(C_numerator, C_denominator))  
 
         max_index = solutions[np.argmax(mu[solution_mask])]
         max_mu = mu[max_index]
@@ -105,25 +110,19 @@ def compute_mip_batch_topk_ver2_warm_nb(atoms, query, sigma, delta, batch_size=1
         if len(solutions) <= k - found_indices_num:
             best_ind[found_indices_num: found_indices_num + len(solutions)] = solutions
             found_indices_num += len(solutions)
-            solution_mask = np.logical_xor(solution_mask_before,
-                                            solution_mask)  # TODO: Does xor work even in the worst case scenario? or should we revive all candidates?
+            solution_mask = np.logical_xor(solution_mask_before, solution_mask)
             solutions = np.nonzero(solution_mask)[0]
             max_index = solutions[np.argmax(mu[solution_mask])]
             max_mu = mu[max_index]
 
-    # need to check if this is correct?
+
     if found_indices_num < k:
         mu_exact = np.multiply(d_used, mu)
-
-
         for i, atom_index in enumerate(solutions):
             mu_exact[i] += atoms[atom_index, d_used[atom_index]:] @ query[d_used[atom_index]:]
 
         d_used[solutions] = dim
-
         mu = np.divide(mu_exact, d_used)
-
-        # TODO: is there a way to avoid copy?
         mu_exact_search = mu_exact.copy()
 
         while found_indices_num < k:
@@ -135,7 +134,7 @@ def compute_mip_batch_topk_ver2_warm_nb(atoms, query, sigma, delta, batch_size=1
     return best_ind, n_samples, mu, d_used
 
 @njit
-def estimate_softmax_normalization_warm_nb(atoms, query, beta, epsilon, delta, sigma, bruteforce=False):
+def estimate_softmax_normalization_warm_nb(atoms, query, beta, epsilon, delta, sigma, bruteforce=False, verbose=False):
     #TODO: when T0=d, return bruteforce
     true_mu = atoms @ query
 
@@ -143,67 +142,62 @@ def estimate_softmax_normalization_warm_nb(atoms, query, beta, epsilon, delta, s
     d = query.shape[0]
     used_samples = 0
 
-    #TODO: do this on other int functions
     T0 = int(np.ceil(min(np.ceil(17 * beta ** 2 * sigma ** 2 * np.log(6 * n / delta)), d)))
-    print("T0:", T0)
+    if verbose:
+        print("T0:", T0)
 
     if T0 == d:
         mu = (atoms @ query).astype(np.float64)
-        #TODO: add beta to errors
-        first_order_error = np.sum(np.multiply(np.exp(mu), (true_mu - mu)))
-        second_order_error = np.sum(np.multiply(np.exp(mu), (true_mu - mu)**2))
         mu -= np.max(mu)
         S_hat = np.sum(np.exp(mu))
-        print(np.full((n,), d).dtype)
-        print("first order error:", first_order_error)
-        print("second order error:", second_order_error)
+
+        if verbose:
+            #TODO: add beta to errors
+            first_order_error = np.sum(np.multiply(np.exp(mu), (true_mu - mu)))
+            second_order_error = np.sum(np.multiply(np.exp(mu), (true_mu - mu)**2))
+            print(np.full((n,), d).dtype)
+            print("first order error:", first_order_error)
+            print("second order error:", second_order_error)
+
         return S_hat, mu, np.full((n,), d).astype(np.int64)
 
     mu_hat = (d / T0) * (atoms[:, :T0] @ query[:T0])
     C = (2 * sigma ** 2 * np.log(6 * n / delta) / T0) ** 0.5
 
-    C_true = (2 * sigma ** 2 * np.log(6 * n / delta) / d) ** 0.5
-
-    true_alpha = np.exp(true_mu - C_true) / np.sum(np.exp(true_mu - C_true))
-    true_gamma = np.exp((true_mu - C_true) / 2) / np.sum(np.exp((true_mu - C_true) / 2))
-
-    #Maybe this is better?
-    #mu_hat -= np.min(mu_hat)
-
     mu_hat_aux = (mu_hat - C) * beta
     #TODO: Add sophisticated numericall stability measure
-    #mu_hat_aux -= np.max(mu_hat_aux)
+    mu_hat_aux -= np.max(mu_hat_aux)
     mu_hat_exp_alpha = np.exp(mu_hat_aux)
     alpha = mu_hat_exp_alpha / np.sum(mu_hat_exp_alpha)
-
     mu_hat_exp_gamma = np.exp(mu_hat_aux / 2)
     gamma = mu_hat_exp_gamma / np.sum(mu_hat_exp_gamma)
-
-
     #seperate this from constructing alpha and gamma(maximum element subtraction)
-    #import ipdb; ipdb.set_trace()
     Term1 = 17 * np.log((6 * n) / delta)
     Term2_constant = (16 * (2 ** 0.5) * np.log((6 * n) / delta) * np.sum(mu_hat_exp_gamma)**2) / (epsilon * np.sum(mu_hat_exp_alpha))
     Term3_constant = (16 * np.log(12 / delta)) / (epsilon ** 2)
 
-    print("ratio:", np.sum(mu_hat_exp_gamma)**2 / (np.sum(mu_hat_exp_alpha)))
+    if verbose:
+        print("ratio:", np.sum(mu_hat_exp_gamma)**2 / (np.sum(mu_hat_exp_alpha)))
 
     Term2 = gamma * Term2_constant
     Term3 = alpha * Term3_constant
 
-    print("T1:", Term1)
-    print("T2:", Term2)
-    print("T3:", Term3)
-    print("Sums:", n*Term1, np.sum(Term2), np.sum(Term3))
+    if verbose:
+        print("T1:", Term1)
+        print("T2:", Term2)
+        print("T3:", Term3)
+        print("Sums:", n*Term1, np.sum(Term2), np.sum(Term3))
 
-    #probably n_samples = max(Term1, Term2, Term3)
+    #TODO: probably n_samples = max(Term1, Term2, Term3)
     n_samples = (Term2 + Term3) + Term1
     n_samples.astype(np.int64)
-    print("prior scaling:", n_samples)
-    #TODO: add beta
-    print("estimate n_i:", sigma**2 * n_samples)
-    print("T:", np.sum(sigma**2 * n_samples))
-    n_samples = np.ceil(np.minimum(beta**2 * sigma**2 * n_samples, d)).astype(np.int64) #TODO: Note that we're ignoring beta here, need to add support later. / Redundant typing?
+    n_samples = np.ceil(np.minimum(beta**2 * sigma**2 * n_samples, d)).astype(np.int64)
+
+    if verbose:
+        print("prior scaling:", n_samples)
+        #TODO: add beta
+        print("estimate n_i:", sigma**2 * n_samples)
+        print("T:", np.sum(sigma**2 * n_samples))
 
     #n_samples = np.ceil(np.minimum((alpha + gamma) * T + T0, d)).astype(np.int64)
 
@@ -217,15 +211,13 @@ def estimate_softmax_normalization_warm_nb(atoms, query, beta, epsilon, delta, s
             mu_hat_refined_aux = d * atoms[i, T0:n_samples[i]] @ query[T0:n_samples[i]]
             mu_hat_refined[i] = (mu_hat[i] * T0 + mu_hat_refined_aux) / max(n_samples[i], 1)
 
-    #normalize only on arms that are NOT bruteforces
 
-    first_order_error = np.sum(np.multiply(np.exp(mu_hat_refined), (true_mu - mu_hat_refined)))
-    second_order_error = np.sum(np.multiply(np.exp(mu_hat_refined), (true_mu - mu_hat_refined)**2))
-
-    print("first order error:", first_order_error / np.sum(np.exp(beta * true_mu)))
-    print("second order error:", second_order_error / np.sum(np.exp(beta * true_mu)))
-
-    print(mu_hat_refined - true_mu)
+    if verbose:
+        first_order_error = np.sum(np.multiply(np.exp(mu_hat_refined), (true_mu - mu_hat_refined)))
+        second_order_error = np.sum(np.multiply(np.exp(mu_hat_refined), (true_mu - mu_hat_refined)**2))
+        print("first order error:", first_order_error / np.sum(np.exp(beta * true_mu)))
+        print("second order error:", second_order_error / np.sum(np.exp(beta * true_mu)))
+        print(mu_hat_refined - true_mu)
 
     #TODO: define seperate vector for mu - max(mu) / mu(untouched)
     mu_hat_refined -= np.max(mu_hat_refined)
@@ -239,63 +231,34 @@ def estimate_softmax_normalization_warm_nb(atoms, query, beta, epsilon, delta, s
 
 # adaSoftmax with warm start
 @njit
-def ada_softmax_nb(A, x, beta, epsilon, delta, n_sigma_sample, k):
+def ada_softmax_nb(A, x, beta, epsilon, delta, n_sigma_sample, k, verbose=False):
 
     #TODO: repleace this with empirical bernstein bound, extend "warm start" to the sigma approximation layer
     sigma = approx_sigma_bound_nb(A, x, n_sigma_sample)
-
-    print("sigma:", sigma)
-    #sigma = 6.0
-
+    if verbose:
+        print("sigma:", sigma)
+        
     #TODO: do NOT compute the S_hat here, just compute estimate on mu
     S_hat, mu_hat, budget_vec = estimate_softmax_normalization_warm_nb(A, x, beta, epsilon / 2, delta / 3, sigma)
-
-    print(budget_vec)
-
-    mu = A @ x
-    mu -= np.max(mu)
-    S = np.sum(np.exp(mu))
-    print("S error:", S_hat - S)
-
-    #print("S estimate:", S_hat)
-
-    print("denominator budget:", np.sum(budget_vec))
-
-    #print(mu_hat)
 
     best_index_hat, budget_mip, mu_hat, budget_vec = compute_mip_batch_topk_ver2_warm_nb(A, x, sigma, delta / 3,
                                                                                       batch_size=16, k=k, mu=mu_hat,
                                                                                       budget_vec=budget_vec)
-
-    print("denominator + best arm identification:", np.sum(budget_vec))
-    #print(mu_hat)
-
     best_index_hat = best_index_hat[:k]
 
     n_arm_pull = int(min(
         np.ceil((288 * sigma ** 2 * beta ** 2 * np.log(6 / delta)) / (epsilon ** 2)),
         x.shape[0]
     ))
-
-    #mu_additional = np.empty(k)
-
     #TODO: add an if statement to not sample if no additional budget is needed
     for arm_index in best_index_hat:
         mu_additional = x.shape[0] * A[arm_index, budget_vec[arm_index]: n_arm_pull] @ x[budget_vec[arm_index]: n_arm_pull]
         mu_hat[arm_index] = (mu_hat[arm_index] * budget_vec[arm_index] + mu_additional) / max(budget_vec[arm_index], n_arm_pull, 1)
 
-    #mu_hat[best_index_hat] += np.divide(x.shape[0] * mu_additional, np.maximum(1, n_arm_pull - budget_vec[best_index_hat]))
-
     budget_vec[best_index_hat] = np.maximum(budget_vec[best_index_hat], n_arm_pull)
-
-    #print("total_budget:", np.sum(budget_vec))
-
-    # y_best_hat = np.exp(beta * (mu_best_hat), dtype=np.float64)
     #TODO: use computational trick here
     y_best_hat = np.exp(beta * (mu_hat))
     budget = np.sum(budget_vec)
     z = y_best_hat / S_hat
-
-    #z = z / np.sum(z)
 
     return best_index_hat, z, budget
