2320eb389405d7e3e0697279567845bd2bc010ce
RyanK0902 <txryank@stanford.edu>
2023-11-28 15:25:54 -0800

    added llms folder

--- adaptive_softmax/constants.py
+++ adaptive_softmax/constants.py
@@ -1,12 +1,15 @@
-BATCH_SIZE = 128
+# algorithm constants
 TOP_K = 1
-BETA = 1.0   # this is roughly inverse of sigma 
+BETA = 1.0   
 DEFAULT_EPSILON = 0.1
 DEFAULT_DELTA = 0.01
-PROFILE = False
-RETURN_STAGE_BUDGETS = False  # TODO (@wonjun): this constant unused?
-OPTIMIZE_CONSTANTS = False
-VERBOSE = True     # this just prints out the budgets. set DEBUG flag for more stats
+BATCH_SIZE = 128
+
+# budget constants
+UNI_CONST = 1
+F_ORDER_CONST = 1
+S_ORDER_CONST = 1
+VERBOSE = True  # set DEBUG flag for more stats
 
 # debugging constants (will write to log)
 DEBUG = True
@@ -14,7 +17,4 @@ DEV_BY = 1  # std deviations
 DEV_RATIO = 0.5 # setting smaller leads to more outliers
 NUM_BINS = 10
 
-# constants for budget  
-UNI_CONST = 1
-F_ORDER_CONST = 1
-S_ORDER_CONST = 1
+# test script constants
---
+++ llms/gpt2/reproduce_wikitext.py
@@ -0,0 +1,42 @@
+import torch
+from tqdm import tqdm
+from transformers import GPTNeoForCausalLM, GPT2TokenizerFast
+from datasets import load_dataset
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("device is ", device)
+model_id = "EleutherAI/gpt-neo-2.7B"  # perplexity should be 11.39
+model = GPTNeoForCausalLM.from_pretrained(model_id).to(device)
+tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
+
+test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt")
+max_length = model.config.max_length
+stride = 512
+seq_len = encodings.input_ids.size(1)
+
+nlls = []
+prev_end_loc = 0
+for begin_loc in tqdm(range(0, seq_len, stride)):
+    end_loc = min(begin_loc + max_length, seq_len)
+    trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+    input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+    target_ids = input_ids.clone()
+    target_ids[:, :-trg_len] = -100
+
+    with torch.no_grad():
+        outputs = model(input_ids, labels=target_ids)
+
+        # loss is calculated using CrossEntropyLoss which averages over valid labels
+        # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+        # to the left by 1.
+        neg_log_likelihood = outputs.loss
+
+    nlls.append(neg_log_likelihood)
+
+    prev_end_loc = end_loc
+    if end_loc == seq_len:
+        break
+
+ppl = torch.exp(torch.stack(nlls).mean())
+print("perplexity is ", ppl)
