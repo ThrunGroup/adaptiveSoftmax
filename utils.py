48fd82616160007a4745683b2847d7b31e726f17
RyanK0902 <txryank@stanford.edu>
2023-11-28 15:29:26 -0800

    cleanup

--- utils.py
+++
@@ -1,8 +0,0 @@
-
-import matplotlib.pyplot as plt
-
-
-def plot_sigma_distribution(A, x, arm_index=0):
-    num_bins = x.shape[0] ** 0.5
-    elmul = A[arm_index] * x
-    plt.hist(elmul, bins=int(num_bins))
