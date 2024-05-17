1d0e5e306195631b0afeccd3dbe86bd48a9b7b9f
Ryan Kang <txryank@sh03-ln08.stanford.edu>
2024-05-17 16:52:57 -0700

    sh scripts

--- llms/pythia/pythia_constants.py
+++
@@ -1,8 +0,0 @@
-PYTHIA_70M = "EleutherAI/pythia-70m-deduped"
-PYTHIA_160M = "EleutherAI/pythia-160m-deduped"
-PYTHIA_410M = "EleutherAI/pythia-410m-deduped"
-PYTHIA_1B = "EleutherAI/pythia-1b-deduped"
-
-MULTIPLICATIVE_ERROR = 0.1
-DELTA_ERROR = 0.1
-WIKITEXT_BETA = 1.0
